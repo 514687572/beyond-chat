0190b3d4f4a5aee545210e6dc74f90c1acc0112f
安爷 <chenjunan@foxmail.com>
2017-07-21 13:44:49 +0800

    websocket

---
+++ beyond-parent/beyond-chat/src/main/java/com/beyond/net/imessage/IMWebSocketConfigurer.java
@@ -0,0 +1,29 @@
+package com.beyond.net.imessage;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+/**
+ * WebScoket配置处理器
+ * 
+ * @author chenjunan
+ * @Date 2016年01月27日 下午10:40
+ */
+@Component
+@EnableWebSocket
+public class IMWebSocketConfigurer extends WebMvcConfigurerAdapter implements WebSocketConfigurer {
+
+	@Resource
+	public IMWebSocketHandler handler;
+
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(handler, "/ws").addInterceptors(new HandShake());
+		registry.addHandler(handler, "/ws/sockjs").addInterceptors(new HandShake()).withSockJS();
+	}
+
+}
