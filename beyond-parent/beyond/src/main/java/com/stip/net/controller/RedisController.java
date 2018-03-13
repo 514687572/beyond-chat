dfafc5b609bdb7d868512ebe5e439a8b97001cbd
514687572 <514687572@qq.com>
2018-03-13 13:09:18 +0800

    使用lua脚本实现抢购

--- beyond-parent/beyond/src/main/java/com/stip/net/controller/RedisController.java
+++ beyond-parent/beyond/src/main/java/com/stip/net/controller/RedisController.java
@@ -12,10 +12,8 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang.Validate;
 import org.springframework.context.annotation.Scope;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +33,9 @@ public class RedisController {
 	@Resource
 	private ThreadPoolTaskExecutor threadPoolTaskExecutor;
 	
+	private static int threadCount = 1;
+	private volatile long a=100;
+	
 	@RequestMapping(value = "/updateVistiTime.do", method = { RequestMethod.PUT,RequestMethod.GET})
 	public Map<String,Object> updateVistiTime(HttpServletRequest request, HttpServletResponse response){
 		Map<String,Object> jsonResult=new HashMap<String, Object>();
@@ -45,46 +46,14 @@ public class RedisController {
 		return jsonResult;
 	}
 	
-	/*
-	@RequestMapping(value = "/test.do", method = { RequestMethod.PUT,RequestMethod.GET})
-	public Map<String,Object> test(HttpServletRequest request, HttpServletResponse response) throws Exception{
-		String luaScript="local storage = redis.call('get',KEYS[1]) " + 
-				"if  storage ~= false then " + 
-				"	if tonumber(storage) > 0 then " + 
-				"		return redis.call('decr',KEYS[1]) " + 
-				"	else " + 
-				"		return -1 " + 
-				"	end " + 
-				"else " + 
-				"	return redis.call('set',KEYS[1],101) " + 
-				"end";
-		Map<String,Object> jsonResult=new HashMap<String, Object>();
-		String[] args= new String[] {"storage_seckill"};
-		List<String> list=new ArrayList<String>();
-		list.add("storage_seckill");
-		Long a=redisService.executeScript(luaScript,list,args);
-		System.out.println(a);
-		if(a>=0) {
-			goodsService.updateGoodsCount(redisService.getCache("storage_seckill", Integer.class), 1000);
-			jsonResult.put("result","购买成功，还剩"+ a);
-		}else {
-			jsonResult.put("result","秒杀完了");
-		}
-		
-		return jsonResult;
-	}*/
-	
-	static int threadCount = 1;
-	volatile long a=100;
-	
 	@RequestMapping(value = "/test.do", method = { RequestMethod.PUT,RequestMethod.GET})
 	public void seckill(HttpServletRequest request) throws InterruptedException {  
 		Map<String,Object> jsonResult=new HashMap<String, Object>();
-			StopWatch watch = new StopWatch();
+			//StopWatch watch = new StopWatch();
 	        final CountDownLatch latch = new CountDownLatch(threadCount);  
 	        String id=request.getParameter("id");
 	        final String count=request.getParameter("count");
-        	watch.start();  
+        	//watch.start();  
 	        
         	threadPoolTaskExecutor.execute(new Thread() {  
 	                public void run() {  
@@ -92,7 +61,6 @@ public class RedisController {
 	            				"if storage ~= false then " + 
 	            				"	if storage ~= nil then " + 
 	            				"		if (tonumber(storage) > 0 and tonumber(storage)>=tonumber(ARGV[1])) then " + 
-	            				"print(tonumber(storage) > 0 and tonumber(storage)==tonumber(ARGV[1]))"+
 	            				"			if (tonumber(storage) > 0 and tonumber(storage)==tonumber(ARGV[1])) then " + 
 	            				"				redis.call('set',KEYS[1],tonumber(storage)-tonumber(ARGV[1])) " + 
 	            				"				return 1" + 
@@ -114,7 +82,6 @@ public class RedisController {
 	            		List<Serializable> list=new ArrayList<Serializable>();
 	            		list.add("storage_seckill");
 	            		a=redisService.executeScript(luaScript,list,args);
-	            		System.out.println(a);
 	                    latch.countDown();  
 	                }  
 	            }); 
@@ -138,9 +105,9 @@ public class RedisController {
 	        	}
 	        	jsonResult.put("result","秒杀完了");
 	        }
-	        watch.stop();  
+	        //watch.stop();  
 	          
-	        System.err.println("time:" + watch.getTotalTimeSeconds());  
+	       // System.err.println("time:" + watch.getTotalTimeSeconds());  
 	    } 
 	
 }
