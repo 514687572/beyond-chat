2d425001ffab26af4cb8e4b3af1b9b6b85cc8232
安爷 <chenjunan@foxmail.com>
2018-04-04 11:45:43 +0800

    增加计算机视觉模块

---
+++ beyond-parent/beyond-vc/src/main/java/tutorial_code/introduction/documentation/Documentation.java
@@ -0,0 +1,11 @@
+package tutorial_code.introduction.documentation;
+
+public class Documentation {
+
+    public static void main (String[] args) {
+
+        //! [hello_world]
+        System.out.println ("Hello World!");
+        //! [hello_world]
+    }
+}
